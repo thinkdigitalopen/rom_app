05a92ad6e544309bd3143715cf01f8930dec53f7
Your Name <you@example.com>
2024-09-26 14:23:27 +0530

    raw metiral - opening stock auto

--- rom_app/restaurant_ops_mgmt/doctype/raw_material_only/raw_material_only.js
+++ rom_app/restaurant_ops_mgmt/doctype/raw_material_only/raw_material_only.js
@@ -1,8 +1,16 @@
-// Copyright (c) 2024, Pubs and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Raw Material Only", {
+	refresh(frm) {
 
-// frappe.ui.form.on("Raw Material Only", {
-// 	refresh(frm) {
+	},
+	opening_stock : function(frm) {
+		console.log('opening_stock');
+		let opening_amount = frm.doc.price * frm.doc.opening_stock;
+	    frm.set_value("opening_amount", opening_amount);
+	},
+	price : function(frm) {
+		console.log('price');
+		let opening_amount = frm.doc.price * frm.doc.opening_stock;
+	    frm.set_value("opening_amount", opening_amount);
+	},
 
-// 	},
-// });
+});
