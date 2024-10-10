16ecad3f529772aeaf23e4b8ec8ab5451819df84
Your Name <you@example.com>
2024-10-10 20:11:38 +0530

    raw mat for indent - enable unit selection

--- rom_app/hooks.py
+++ rom_app/hooks.py
@@ -175,11 +175,11 @@ app_include_js = ["/assets/rom_app/js/d3.v6.min.js",
 # Scheduled Tasks
 # ---------------
 
-scheduler_events = {
-    "cron": {
-        "*/5 * * * *": ["rom_app.scheduled_tasks.inventory_summary"],
-    }
-    }
+# scheduler_events = {
+#     "cron": {
+#         "*/5 * * * *": ["rom_app.scheduled_tasks.inventory_summary"],
+#     }
+#     }
 # "59 23 * * *": ["rom_app.scheduled_tasks.inventory_summary"],
 # Testing
 # -------
